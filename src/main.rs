3495b8fdb7c7c8bdee5084de56a28cc900081ac5
Gianmarco Donnici <gianmarco.donnici@gmail.com>
2019-10-28 17:41:57 +0100

    rewrote a couple comments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![no_std]      // no std where we're going kid!
-#![no_main]     // main is for suckers
+#![no_main]     // main is for suckers, disabled for a custom entry point
 
 /// This function is called on panic.
 use core::panic::PanicInfo;
@@ -8,7 +8,7 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]    // keep the name, otherwise rust renames this function to a unique name
-pub extern "C" fn _start() -> ! {   // use the C calling convention, to have a valid entry point
+#[no_mangle]    // don't mangle, the linker will look for a function named _start
+pub extern "C" fn _start() -> ! {   // use the C calling convention
     loop {}
 }
