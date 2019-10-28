69c463a0840aa28a2bb4388f548c3022c2159ae9
Gianmarco Donnici <gianmarco.donnici@gmail.com>
2019-10-28 17:15:37 +0100

    disabled stack unwinding

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 #![no_std]  //no std where we're going kid!
 
+/// This function is called on panic.
+use core::panic::PanicInfo;
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    loop {}
+}
+
 fn main() {
     // stuff
 }
