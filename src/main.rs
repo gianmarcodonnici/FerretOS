b624f1ad8d3ed41e0b9a9419041c180db36d98ed
Gianmarco Donnici <gianmarco.donnici@gmail.com>
2019-10-28 17:35:31 +0100

    removed main, added new entry point

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-#![no_std]  //no std where we're going kid!
+#![no_std]      // no std where we're going kid!
+#![no_main]     // main is for suckers
 
 /// This function is called on panic.
 use core::panic::PanicInfo;
 #[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-fn main() {
-    // stuff
+#[no_mangle]    // keep the name, otherwise rust renames this function to a unique name
+pub extern "C" fn _start() -> ! {   // use the C calling convention, to have a valid entry point
+    loop {}
 }
