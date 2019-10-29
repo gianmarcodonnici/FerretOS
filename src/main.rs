bc09aae6275a0852b7da4ce6cbe6adc6a7ce4317
Gianmarco Donnici <gianmarco.donnici@gmail.com>
2019-10-29 19:47:06 +0100

    print macros implemented, _start now uses println

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,8 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]    //don't mangle, the linker will look for a function named _start
 pub extern "C" fn _start() -> ! {   //use the C calling convention
-    let vga_buffer = 0xb8000 as *mut u8;    //vga buffer is at this address
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World");
     loop {}
 }
